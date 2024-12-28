9aa6d5df70d3004c7c573e6f445efd731f88f649
Ronald Remmerswaal <ronaldremmerswaal@pm.me>
2024-12-28 16:37:08 +0100

    wip init: compute poly approx

--- src/initialization.jl
+++ src/initialization.jl
@@ -2,7 +2,10 @@
     V = volume(Φ::Function, c::Ngon)
 
 Compute the volume V occupied by the reference phase. I.e. compute the volume of the set
-    𝕊 = {𝐱 ∈ c | Φ(𝐱) ≤ 0}.
+    𝕊 = {𝐱 ∈ c | Φ(𝐱) ≤ 0},
+or
+    𝕊 = {𝐱 ∈ c | Φ(𝐱)}
+if Φ is boolean.
 """
 function volume(Φ::Function, c::Ngon)
     verts = c.vertices
@@ -12,22 +15,56 @@ function volume(Φ::Function, c::Ngon)
 
     if T == Bool
         # Here Φ(𝐱) == true ⟺ 𝐱 ∈ 𝕊, and we must use bisection on a shifted function
-        root_finder = Roots.Bisection()
+        Method = Roots.Bisection
         Φrf(p) = Φp(p) - .5
     else
-        root_finder = Roots.Brent()
+        Method = Roots.Brent
         Φrf = Φp
     end
 
     Φverts = Φrf.(verts)
-    inside_verts = Φverts ≤ 0
+    inside_verts = Φverts .≤ 0
 
     # Quick return for trivial cases
     if all(inside_verts)
         return measure(c)
-    elseif all(!inside_verts)
+    elseif all(.!inside_verts)
         return 0u"m^2"
     end
 
     # TODO: compute polygonal approx of reference phase inside c
+    ϕc_approx, edge_is_hf = ngon_approx(Φrf, c, inside_verts, Method)
+end
+
+function ngon_approx(Φ::Function, c::Ngon, inside_verts::AbstractVector{Bool}, Method)
+    verts = c.vertices
+
+    first_vdx = findfirst(inside_verts)
+    vdx = first_vdx
+
+    new_verts = Vector{eltype(verts)}()
+    edge_is_hf = Vector{Bool}()
+
+    for _ = 1 : length(verts)
+        next_vdx = vdx == length(verts) ? 1 : vdx + 1
+
+        if inside_verts[vdx]
+            push!(new_verts, verts[vdx])
+            push!(edge_is_hf, false)
+        end
+
+        if inside_verts[vdx] != inside_verts[next_vdx]
+            # Compute the new vertex
+            v1 = verts[vdx]
+            v2 = verts[next_vdx]
+            v(s) = v1 + (v2 - v1) * s
+
+            push!(new_verts, v(find_zero(Φ ∘ v, (0, 1), Method())))
+            push!(edge_is_hf, inside_verts[vdx])
+        end
+
+        vdx = next_vdx
+    end
+
+    return new_verts, edge_is_hf
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,13 @@
 using GeometricVOF
+using Meshes
+using Unitful
 using Test
 
 @testset "GeometricVOF.jl" begin
-    # Write your tests here.
+    @testset "initialization" begin
+        t = Triangle((0.0, 0.0), (1.0, 0.0), (0.0, 1.0))
+        Φ(x, y) = ustrip(x) - 0.5
+        GeometricVOF.volume(Φ, t)
+    end
+
 end
