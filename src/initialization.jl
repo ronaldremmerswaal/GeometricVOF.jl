aa73e4ac02db755c10ff42758ca82599dd253a29
Ronald Remmerswaal <ronaldremmerswaal@pm.me>
2024-12-28 18:48:29 +0100

    wip parabolic test

--- src/initialization.jl
+++ src/initialization.jl
@@ -86,15 +86,15 @@ function hf_measure(Φ::Function, v1::Point, v2::Point, Method)
     𝛕 = v1 - v2
     𝛈 = Vec(𝛕[2], -𝛕[1]) # Outward pointing normal (in hf direction)
 
-    # The levelset function in local coordinates
+    # The levelset function in local coordinates on τ ∈ [0, 1]
     Φl(τ, η) = Φ(v2 + τ * 𝛕 + η * 𝛈)
 
     # For each τ, the value of the height-function results from solving a rootfinding problem
     hf(τ) = find_zero(η -> Φl(τ, η), (-ηmax, ηmax), Method())
 
     τ_gl, weight_gl = gausslegendre(10) # TODO: precompute?
+    τ_gl = (τ_gl .+ 1)/2
     h = norm(𝛕)
 
     return h^2 * sum(hf.(τ_gl) .* weight_gl)
-
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,8 +7,13 @@ using Test
     @testset "levelset ∩ ngon" begin
         # Linear
         t = Triangle((0.0, 0.0), (1.0, 0.0), (0.0, 1.0))
-        Φ(x, y) = x - 0.5u"m"
-        @test GeometricVOF.measure(Φ, t) == (.5 - 1/8) * u"m^2"
+        Φl(x, y) = x - 0.5u"m"
+        @test GeometricVOF.measure(Φl, t) == .5(1 - 1/4)u"m^2"
+
+        # Parabolic
+        q = Quadrangle((0.0, 0.0), (1.0, 0.0), (1.0, 1.0), (0.0, 1.0))
+        Φp(x, y) = y - (.5 + .25(x / u"1m")^2)u"m"
+        @test GeometricVOF.measure(Φp, q) == (1/2 + 1/12)u"m"
     end
 
 end
